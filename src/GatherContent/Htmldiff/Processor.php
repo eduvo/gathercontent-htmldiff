7358b570be4995a8445aa77343b611936a864ac3
cyberstz <skizzord@gmail.com>
2024-01-22 11:05:01 +0100

    Ignore start end leafs

--- src/GatherContent/Htmldiff/Processor.php
+++ src/GatherContent/Htmldiff/Processor.php
@@ -105,11 +105,11 @@ class Processor implements ProcessorInterface
 
                 if ($leaf == 'start') {
 
-                    $html .= $this->openTag($path);
+                    // $html .= $this->openTag($path);
 
                 } elseif ($leaf == 'end') {
 
-                    $html .= $this->closeTag($path);
+                    // $html .= $this->closeTag($path);
 
                 } else {
 
